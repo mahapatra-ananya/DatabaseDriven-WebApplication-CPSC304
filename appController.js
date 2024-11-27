261c756f9dc17ed2ef062d746b55cd8bb5842dda
aliko18 <aliko18@student.ubc.ca>
2024-11-27 14:04:39 -0800

    Moved join-server.html into root public folder

--- appController.js
+++ appController.js
@@ -95,6 +95,58 @@ router.post('/serverpage-channels', async (req, res) => {
     }
 });
 
+////////////////////GO TO A SERVER PAGE ////////////////////////////////////
+router.post('/server', async (req, res) => {
+    const { ServerID } = req.body;
+    res.redirect(`/server.html?serverid=${encodeURIComponent(ServerID)}`)
+});
+
+
+/////////////////// JOIN SERVER - LIST FILTERED SERVERS ////////////////////
+router.post('/join-server-list', async (req, res) => {
+    const { Username } = req.body;
+
+    // STEP 1: SELECT the servers the current user has NOT joined AND NOT an admin of
+    const filteredUserServers = await allisonAppService.fetchFilteredUserServers(Username);
+    console.log(`appController: FilteredUserServers: ${filteredUserServers}`)
+
+    if (filteredUserServers) {
+        res.json({ success: true, filteredServers: filteredUserServers});
+    } else {
+        res.status(500).json({ success: false });
+    }
+});
+
+/* INSERT JOIN SERVER AND GENERAL MEMBER*/
+
+router.post("/insert-joins-table", async (req, res) => {
+    const { Username, ServerID } = req.body;
+
+    const insertResult = await allisonAppService.insertJoinsTable(Username, ServerID);
+    if (insertResult) {
+        res.redirect(`/server.html?serverid=${encodeURIComponent(ServerID)}`)
+    } else {
+        res.status(500).json({ success: false });
+    }
+});
+
+router.post("/insert-general-member-table", async (req, res) => {
+    const { Username } = req.body;
+
+    const checkUserIsGeneralMember = await allisonAppService.isUserGeneralMember(Username)
+    console.log(`isusergeneralmember: ${checkUserIsGeneralMember}`)
+    let insertResult
+    if (!checkUserIsGeneralMember) {
+        insertResult = await allisonAppService.insertGeneralMemberTable(Username);
+    }
+
+    if (insertResult || checkUserIsGeneralMember) {
+        res.json({ success: true});
+    } else {
+        res.status(500).json({ success: false });
+    }
+});
+
 router.post('/server/calendar', async (req, res) => {
     const { CalendarID } = req.body;
     res.redirect(`/Calendar.html?calendarid=${encodeURIComponent(CalendarID)}`)
--- public/Allison/join-server-script.js
+++ public/Allison/join-server-script.js
@@ -1,9 +1,146 @@
+// TODO: UPDATE username
+const USERNAME = 'GojoSatoru'
 
-const joinServerBtns = () => {
-    // get every join server button
+async function insertJoinServerAndGo(event) {
+    event.preventDefault();
+
+    // Get the selected server's ID
+    const joinServerID = event.target.value;
+
+    // Insert into join server table and immediately go to the server's page
+    const generalMemberResponse = await fetch('/insert-general-member-table', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            Username: USERNAME,
+        })
+    });
+
+    const generalMemberResponseData = await generalMemberResponse.json();
+    if (generalMemberResponseData.success) {
+        console.log('success inserting user in general member table')
+    } else {
+        alert('Error becoming a general member')
+        return;
+    }
+
+    // Insert into joins
+    const insertJoinsResponse = await fetch('/insert-joins-table', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            Username: USERNAME,
+            ServerID: joinServerID,
+        })
+    });
+
+    // const insertJoinsResponseData = await insertJoinsResponse.json();
+    if (insertJoinsResponse.redirected) {
+        console.log('success inserting user in joins table')
+        // window.location.href = insertJoinsResponse.url;
+        const insertServerGroup = document.querySelector('#serverList');
+        insertServerGroup.textContent = '';
+        const successMsg = document.createElement('p')
+        successMsg.textContent = `You have successfully joined the server! Would you like to go to the server page?`
+        const goToServerBtn = document.createElement('button');
+        goToServerBtn.textContent = 'Go to Server';
+        goToServerBtn.addEventListener('click', async function () {
+            const response = await fetch('/server', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    ServerID: joinServerID,
+                })
+            });
+
+            if (response.redirected) {
+                window.location.href = response.url;
+            } else {
+                alert('failed to redirect')
+            }
+        })
+        insertServerGroup.appendChild(successMsg)
+        insertServerGroup.appendChild(goToServerBtn)
+    } else {
+        alert('Error joining Server')
+        return;
+    }
 }
 
-//TODO: TEMPORARY JUST FOR SHOW
-document.querySelector(".joinServerBtn").addEventListener('click', () => {
-    alert("TODO: Implement Join Button Clicked")
-})
+async function fetchAndDisplayFilteredServers() {
+    const insertServerGroup = document.querySelector('#serverList');
+
+    const response = await fetch('/join-server-list', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            Username: USERNAME,
+        })
+    });
+
+    const responseData = await response.json();
+    const filteredServers = responseData.filteredServers;
+    console.log(filteredServers);
+
+
+
+    // Always clear old, already fetched data before new fetching process.
+    if (insertServerGroup) {
+        insertServerGroup.innerHTML = '';
+    }
+
+    if (filteredServers.length > 0) {
+        filteredServers.forEach(server => {
+
+            const serverItem = document.createElement('div');
+            serverItem.className = 'serverItem';
+            const serverName = document.createElement('p')
+            serverName.className = 'serverName'
+            serverName.textContent = server[1];
+            const serverId = document.createElement('p')
+            serverId.className = 'serverId'
+            serverId.textContent = `ID: ${server[0]}`;
+            const joinServerBtn = document.createElement('button')
+            joinServerBtn.className = 'joinServerBtn'
+            joinServerBtn.textContent = 'Join';
+            joinServerBtn.value = server[0]
+            joinServerBtn.addEventListener('click', insertJoinServerAndGo);
+
+            serverItem.appendChild(serverName);
+            serverItem.appendChild(serverId);
+            serverItem.appendChild(joinServerBtn);
+
+            serverList.appendChild(serverItem);
+        });
+    } else {
+        const noAvailableServersMsg = document.createElement('p');
+        noAvailableServersMsg.textContent ='There are currently no servers available for you to join. Please come back later!'
+        insertServerGroup.appendChild(noAvailableServersMsg)
+    }
+}
+
+
+// ---------------------------------------------------------------
+// Initializes the webpage functionalities.
+// Add or remove event listeners based on the desired functionalities.
+window.onload = function() {
+    // checkDbConnection();
+
+    fetchTableData();
+
+};
+
+
+// General function to refresh the displayed table data.
+// You can invoke this after any table-modifying operation to keep consistency.
+function fetchTableData() {
+    fetchAndDisplayFilteredServers();
+}
